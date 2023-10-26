88f1f3d46460c192f3e12555ecc0c69b9eace61f
Chris Stepien <Chris.Stepien@outlook.com>
2023-10-26 00:06:37 -0600

    Implementing reading directory path
    Checks directory for every file and inputs into a list

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -1,12 +1,34 @@
 # Python program to organize files on the desktop into folders
 
 # importing os module
-import os 
+from pathlib import Path
 
 # importing shutil module
 import shutil
 
 # desktop path
-path = 'C:/Users/chris/OneDrive/Desktop'
+origin_path = Path('C:/Users/chris/OneDrive/Desktop')
+destination_path = Path('C:/Users/chris/OneDrive/Desktop')
 
-print(os.listdir(path))
+def main():
+    
+   unsorted_files = sorting_path_files(origin_path)
+   print(unsorted_files) 
+    
+def sorting_path_files(directory: Path):     
+    
+    file_list = []
+    
+    for item in directory.iterdir():
+        if item.is_file():
+            
+            file_list.append(item)
+    
+    return file_list
+    
+# create folders function
+
+# import files into folders    
+
+if __name__ == "__main__":
+    main()    
