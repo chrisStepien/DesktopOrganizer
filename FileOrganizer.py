d9cc8aee66b7e25545b9e1f0c1fe00c6ec304ce6
Chris Stepien <Chris.Stepien@outlook.com>
2023-10-26 00:56:04 -0600

    Implemented getting files and creating folders

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -1,10 +1,9 @@
 # Python program to organize files on the desktop into folders
 
-# importing os module
-from pathlib import Path
-
-# importing shutil module
+# importing required modules
+import os  
 import shutil
+from pathlib import Path
 
 # desktop path
 origin_path = Path('C:/Users/chris/OneDrive/Desktop')
@@ -12,23 +11,52 @@ destination_path = Path('C:/Users/chris/OneDrive/Desktop')
 
 def main():
     
-   unsorted_files = sorting_path_files(origin_path)
-   print(unsorted_files) 
+   unsorted_files = getting_files(origin_path)
+   create_folders(destination_path, unsorted_files)
+   setting_files(unsorted_files)
+
+# returns a list of all files in the given directory path         
+def getting_files(directory: Path):     
     
-def sorting_path_files(directory: Path):     
-    
-    file_list = []
+    master_file_list = []
     
     for item in directory.iterdir():
-        if item.is_file():
+        if item.is_file() and os.path.splitext(item)[-1].lower() != '.ini':
             
-            file_list.append(item)
-    
-    return file_list
-    
-# create folders function
+            master_file_list.append(item)
+            
+    return master_file_list
 
-# import files into folders    
+# creates a folder for each file format    
+def create_folders(path, file_list):
+    
+    extension_list = []
+    
+    for file in file_list:
+        
+        # split the extension from the path and normalise it to lowercase
+        file_ext = os.path.splitext(file)[-1].lower()
+        extension_list.append(file_ext)
+
+    # sorts out duplicates
+    sorted_ext_list = sorted(set(extension_list))
+    
+    for ext in sorted_ext_list:
+        
+        # removes decimal from exstension and upper cases the remaining
+        new_folder_path = path / (ext.replace('.', '').upper() + " Files")
+        
+        try:
+            new_folder_path.mkdir(parents=True, exist_ok=False)
+        except FileExistsError:
+            print("Folder is already there")
+        else:
+            print("Folder was created")        
+
+# import files into folders
+def setting_files(unsorted_files):
+    print(unsorted_files)
+    
 
 if __name__ == "__main__":
     main()    
